7361490cd11e3c71fa5790d0c71f6877f851d65a
ZachWi <constellationagram@gmail.com>
2023-10-16 00:12:12 -0400

    New fibonacci thing, optimized up to about n = 1000.

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+long double fibIter(long double n) {
+    if (n <= 1)
+        return n;
+
+    long double prev = 0, current = 1, temp;
+    for (int i = 2; i <= n; i++) {
+        temp = current;
+        current = prev + current;
+        prev = temp;
+    }
+    return current;
+}
+
+int main() {
+    long double n;
+    printf("n: ");
+    scanf("%Lf", &n);
+    printf("fib(%Lf) = %Lf\n", n, fibIter(n));
+    return 0;
+}
